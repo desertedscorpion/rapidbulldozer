4afdd3193162688f23f405acf48b74063de4f735
Emory Merryman <emory.merryman@gmail.com>
2016-07-04 17:55:32 +0000

    fixed error in save gnucash script

--- images/cloud9/950-bin-gnucash.sh
+++ images/cloud9/950-bin-gnucash.sh
@@ -5,7 +5,7 @@ cd $(mktemp -d) &&
     tar --create --file gnucash-${TSTAMP}.tar --directory ${HOME} gnucash &&
     gzip -9 gnucash-${TSTAMP}.tar &&
     gpg2 --encrypt --recipient "2D2D81DA" --sign gnucash-${TSTAMP}.tar.gz &&
-    gdrive upload --parent "0B5yjkaCDthrQUWdZNEZJMlNTME0" gnucash-${TSTAMP}.tar.gz &&
+    gdrive upload --parent "0B5yjkaCDthrQUWdZNEZJMlNTME0" gnucash-${TSTAMP}.tar.gz.gpg &&
     cd $(mktemp -d) &&
     read -p "FILE ID:  " FILE_ID &&
     gdrive download "${FILE_ID}" &&
