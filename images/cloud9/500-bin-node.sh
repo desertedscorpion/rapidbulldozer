57c069fb95644443eb6c796e8a73d34706caac87
Emory Merryman <emory.merryman@gmail.com>
2016-07-04 20:28:16 +0000

    I can use this to edit with live-analytics-visualiztion with the following in mind:
    (0) sudo nginx
    (1) user must git fetch origin && git checkout develop
    (2) it is supposed to register npm, the script works but it is not happening automatically, must call /opt/rapidbulldozer/init/bin/500-bin-node.sh
    (3) npm install -- takes a while
    (4) gulp

---
+++ images/cloud9/500-bin-node.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo PATH=/opt/rapidbulldozer/node-v6.2.0-linux-x64/bin:\${PATH} >> ${HOME}/.bashrc &&
+PATH=/opt/rapidbulldozer/node-v6.2.0-linux-x64/bin:${PATH} &&
+npm set registry https://npm.363-283.io &&
+npm set cafile /opt/rapidbulldozer/ca.crt &&
+npm config set strict-ssl false &&
+jspm config strictSSL false &&
+source /opt/rapidbulldozer/properties/ldap.properties &&
+echo -e "${LDAP_USERNAME}\n${LDAP_PASSWORD}\n${LDAP_EMAIL}" | npm adduser --registry https://npm.363-283.io
+true
