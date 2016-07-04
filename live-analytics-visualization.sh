e79f08bee758b01edaa5978e2d881a031bb8d9b0
Emory Merryman <emory.merryman@gmail.com>
2016-07-04 18:46:08 +0000

    live-analytics-visualization.sh

--- live-analytics-visualization.sh
+++ live-analytics-visualization.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-PORT=${@} &&
+CLOUD9_PORT=${1} &&
+NGINX_PORT=${2} &&
 VERSION=$(cat version) &&
 export GPG_SECRET_KEY="$(cat ${HOME}/.private/secret.key)" &&
 export GPG_OWNER_TRUST="$(cat ${HOME}/.private/owner.trust)" &&
@@ -15,5 +16,5 @@ export ORIGIN_URL=ssh://git@gitlab.363-283.io:2252/emory.merryman/live-analytics
 export DISPLAY=$(ifconfig | grep "inet.*netmask" | grep -v "127.0.0.1" | cut -f2 -d " "):0 &&
 export EMAIL=emory.merryman@gmail.com &&
 export NAME="Emory Merryman" &&
-docker run -it --rm --env GPG_SECRET_KEY="${GPG_SECRET_KEY}" --env GPG_OWNER_TRUST="${GPG_OWNER_TRUST}" --env ID_RSA="${ID_RSA}" --env PASS_HOST="${PASS_HOST}" --env PASS_URL="${PASS_URL}" --env CANONICAL_HOST="${CANONICAL_HOST}" --env ORIGIN_HOST="${ORIGIN_HOST}" --env CANONICAL_URL="${CANONICAL_URL}" --env ORIGIN_URL="${ORIGIN_URL}" --env DISPLAY="${DISPLAY}" --env EMAIL="emory.merryman@gmail.com" --env NAME="Emory Merryman" -p ${PORT}:8080  --volume ${HOME}/.private/secret.key:/opt/rapidbulldozer/gpg.secret.key:ro desertedscorpion/cloud9:${VERSION}
+docker run -it --rm --env GPG_SECRET_KEY="${GPG_SECRET_KEY}" --env GPG_OWNER_TRUST="${GPG_OWNER_TRUST}" --env ID_RSA="${ID_RSA}" --env PASS_HOST="${PASS_HOST}" --env PASS_URL="${PASS_URL}" --env CANONICAL_HOST="${CANONICAL_HOST}" --env ORIGIN_HOST="${ORIGIN_HOST}" --env CANONICAL_URL="${CANONICAL_URL}" --env ORIGIN_URL="${ORIGIN_URL}" --env DISPLAY="${DISPLAY}" --env EMAIL="emory.merryman@gmail.com" --env NAME="Emory Merryman" -p ${CLOUD9_PORT}:8080 -p ${NGINX_PORT}:8443  --volume ${HOME}/.private/secret.key:/opt/rapidbulldozer/gpg.secret.key:ro desertedscorpion/cloud9:${VERSION}
 true
