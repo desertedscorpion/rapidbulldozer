ead8bb79f4e70d3b27a277ebabe05f1cd251ecd4
Emory Merryman <emory.merryman@deciphernow.com>
2016-07-04 11:39:38 +0000

    use global commit for git user options

--- images/cloud9/100-bin.sh
+++ images/cloud9/100-bin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source /opt/rapidbulldozer/properties/git.properties &&
-git -C /var/workspace config user.email="${EMAIL}" &&
-git -C /var/workspace config user.name="${NAME}" &&
+git config --global user.email="${EMAIL}" &&
+git config --global user.name="${NAME}" &&
 true
