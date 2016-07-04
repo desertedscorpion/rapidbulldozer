7a68b738a019b249062c383a9e5ec1ff13d3b255
Emory Merryman <emory.merryman@deciphernow.com>
2016-07-04 02:09:45 +0000

    add a workspace volume

--- scratch.sh
+++ scratch.sh
@@ -5,6 +5,7 @@ export GPG_OWNER_TRUST="$(cat ${HOME}/.private/owner.trust)" &&
 export ID_RSA="$(cat ${HOME}/.ssh/id_rsa)" &&
 export PASS_HOST="github.com" &&
 export PASS_URL=git@github.com:AFnRFCb7/lanterngold.git &&
+export WORKSPACE=$(mktemp -d) &&
 docker-compose build scratch &&
 docker-compose up scratch &&
 true
