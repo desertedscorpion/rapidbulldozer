cb7fac8ebb5bbab83838c91b000a6ae520ec3458
Emory Merryman <emory.merryman@deciphernow.com>
2016-07-04 01:30:51 +0000

    use export

--- scratch.sh
+++ scratch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-GPG_SECRET_KEY="$(cat ${HOME}/.private/secret.key)" &&
-GPG_OWNER_TRUST="$(cat ${HOME}/.private/owner.trust)" &&
-ID_RSA="$(cat ${HOME}/.ssh/id_rsa)" &&
-PASS_HOST="github.com" &&
-PASS_URL=git@github.com:AFnRFCb7/lanterngold.git &&
+export GPG_SECRET_KEY="$(cat ${HOME}/.private/secret.key)" &&
+export GPG_OWNER_TRUST="$(cat ${HOME}/.private/owner.trust)" &&
+export ID_RSA="$(cat ${HOME}/.ssh/id_rsa)" &&
+export PASS_HOST="github.com" &&
+export PASS_URL=git@github.com:AFnRFCb7/lanterngold.git &&
 docker-compose build scratch &&
 docker-compose up scratch &&
 true
