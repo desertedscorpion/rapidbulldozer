b41a370e311eb736faab107be8087fb25a80c721
Emory Merryman <emory.merryman@deciphernow.com>
2016-07-04 04:34:12 +0000

    add 2 dummy scripts

--- images/cloud9/scripts/sbin/init-parts.sh
+++ images/cloud9/scripts/sbin/init-parts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mktemp /opt/rapidbulldozer/init/sbin/XXXXXXXX.sh &&
+mktemp /opt/rapidbulldozer/init/bin/XXXXXXXX.sh &&
 chmod 0500 /opt/rapidbulldozer/init/sbin/*.sh &&
 chmod 0555 /opt/rapidbulldozer/init/bin/*.sh &&
 run-parts /opt/rapidbulldozer/init/sbin &&
