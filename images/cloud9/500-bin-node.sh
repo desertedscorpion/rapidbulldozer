5726dab1fbed2d16dc6bbdc075b50bd9174ebddc
Emory Merryman <emory.merryman@gmail.com>
2016-07-04 19:49:18 +0000

    wrong namespace

--- images/cloud9/500-bin-node.sh
+++ images/cloud9/500-bin-node.sh
@@ -5,6 +5,6 @@ npm set registry https://npm.363-283.io &&
 npm set cafile /opt/rapidbulldozer/ca.crt &&
 npm config set strict-ssl false &&
 jspm config strictSSL false &&
-source /opt/needlesslaser/properties/ldap.properties &&
+source /opt/rapidbulldozer/properties/ldap.properties &&
 echo -e "${LDAP_USERNAME}\n${LDAP_PASSWORD}\n${LDAP_EMAIL}" | npm adduser --registry https://npm.363-283.io
 true
