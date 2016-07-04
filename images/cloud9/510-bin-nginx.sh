65a5c2c7b9f42749eae84b750577dc46a7bb8362
Emory Merryman <emory.merryman@gmail.com>
2016-07-04 19:09:00 +0000

    checkout out nginx

---
+++ images/cloud9/510-bin-nginx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git -C /etc/nginx init &&
+    git -C /etc/nginx remote add origin ssh://git@gitlab.363-283.io:2252/emory.merryman/nginx-dev-infrastructure.git &&
+    git -C /etc/nginx fetch origin &&
+    git -C /etc/nginx checkout origin &&
+    true
