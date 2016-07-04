e1de7b142d4d041417f87e27fe9b5f0f4c6157a7
Emory Merryman <emory.merryman@deciphernow.com>
2016-07-04 00:45:15 +0000

    whoops I forgot the path

--- images/cloud9/scripts/bin/init.sh
+++ images/cloud9/scripts/bin/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+export PATH=/opt/rapidbulldozer/scripts/bin:${PATH} &&
 node "/opt/rapidbulldozer/c9sdk/server.js" --listen 0.0.0.0 -w "/var/workspace" -p 8080 --auth user:password &&
--- images/cloud9/scripts/sbin/install.sh
+++ images/cloud9/scripts/sbin/install.sh
@@ -2,6 +2,9 @@
 
 dnf update --assumeyes &&
 dnf install --assumeyes dnf coreutils &&
+chmod 0500 /opt/rapidbulldozer/scripts/sbin/*.sh &&
+chmod 0555 /opt/rapidbulldozer/scripts/bin/*.sh &&
+export PATH=/opt/rapidbulldozer/scripts/sbin:${PATH} &&
 install-user.sh &&
 install-cloud9.sh
 dnf update --assumeyes &&
