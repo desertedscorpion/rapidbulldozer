0f0227a379664c6a7cbf01b27ec2e67d13ca9d9f
Emory Merryman <emory.merryman@gmail.com>
2016-07-04 20:07:18 +0000

    install libpng and checkout nginx develop

--- images/cloud9/510-bin-nginx.sh
+++ images/cloud9/510-bin-nginx.sh
@@ -2,6 +2,6 @@
 
 git -C /etc/nginx init &&
     git -C /etc/nginx remote add origin ssh://git@gitlab.363-283.io:2252/emory.merryman/nginx-dev-infrastructure.git &&
-    git -C /etc/nginx fetch origin &&
+    git -C /etc/nginx fetch origin develop &&
     git -C /etc/nginx checkout develop &&
     true
