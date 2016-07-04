1a6e78d9401136f920446d74d8d8e1991ceca0ed
Emory Merryman <emory.merryman@gmail.com>
2016-07-04 19:30:36 +0000

    fixed an error in nginx install

--- images/cloud9/510-bin-nginx.sh
+++ images/cloud9/510-bin-nginx.sh
@@ -3,5 +3,5 @@
 git -C /etc/nginx init &&
     git -C /etc/nginx remote add origin ssh://git@gitlab.363-283.io:2252/emory.merryman/nginx-dev-infrastructure.git &&
     git -C /etc/nginx fetch origin &&
-    git -C /etc/nginx checkout origin &&
+    git -C /etc/nginx checkout develop &&
     true
