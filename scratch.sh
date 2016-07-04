d673c600b49f36851bbf521f9a3cf55cddf787dc
Emory Merryman <emory.merryman@deciphernow.com>
2016-07-04 03:22:26 +0000

    fixed script no need params

--- scratch.sh
+++ scratch.sh
@@ -6,10 +6,10 @@ export ID_RSA="$(cat ${HOME}/.ssh/id_rsa)" &&
 export PASS_HOST="github.com" &&
 export PASS_URL=git@github.com:AFnRFCb7/lanterngold.git &&
 export WORKSPACE=$(mktemp -d) &&
-export CANONICAL_HOST=${1} &&
-export ORIGIN_HOST=${2} &&
-export CANONICAL_URL=${3} &&
-export ORIGIN_URL=${3} &&
+export CANONICAL_HOST=github.com &&
+export ORIGIN_HOST=github.com &&
+export CANONICAL_URL=git@github.com:desertedscorpion/rapidbulldozer.gitdocker &&
+export ORIGIN_URL=git@github.com:desertedscorpion/rapidbulldozer.gitdocker &&
 export DISPLAY=$(ifconfig | grep "inet.*netmask" | grep -v "127.0.0.1" | cut -f2 -d " "):0
 docker-compose build scratch &&
 docker-compose up scratch &&
