2f36dca431d82e0ec877f6344b7c41d99c7479b8
Emory Merryman <emory.merryman@gmail.com>
2016-07-04 22:23:16 +0000

    only one carriage return in jspm registry config github

--- images/cloud9/520-bin-github.sh
+++ images/cloud9/520-bin-github.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source /opt/rapidbulldozer/properties/github.properties &&
-echo -e "\n\n${GITHUB_ACCESS_TOKEN}\n\n " | jspm registry config github &&
+echo -e "\n${GITHUB_ACCESS_TOKEN}\n\n " | jspm registry config github &&
 true
