8ee169aaa03aba868d5fa2fc66adb73e1cc7a635
Emory Merryman <emory.merryman@gmail.com>
2016-07-04 12:56:39 +0000

    use custom port

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PORT=${@} &&
 VERSION=$(cat version) &&
 export GPG_SECRET_KEY="$(cat ${HOME}/.private/secret.key)" &&
 export GPG_OWNER_TRUST="$(cat ${HOME}/.private/owner.trust)" &&
@@ -14,5 +15,5 @@ export ORIGIN_URL=git@github.com:desertedscorpion/rapidbulldozer.gitdocker &&
 export DISPLAY=$(ifconfig | grep "inet.*netmask" | grep -v "127.0.0.1" | cut -f2 -d " "):0 &&
 export EMAIL=emory.merryman@gmail.com &&
 export NAME="Emory Merryman" &&
-docker run -it --rm --env GPG_SECRET_KEY="${GPG_SECRET_KEY}" --env GPG_OWNER_TRUST="${GPG_OWNER_TRUST}" --env ID_RSA="${ID_RSA}" --env PASS_HOST="${PASS_HOST}" --env PASS_URL="${PASS_URL}" --env CANONICAL_HOST="${CANONICAL_HOST}" --env ORIGIN_HOST="${ORIGIN_HOST}" --env CANONICAL_URL="${CANONICAL_URL}" --env ORIGIN_URL="${ORIGIN_URL}" --env DISPLAY="${DISPLAY}" --env EMAIL="emory.merryman@gmail.com" --env NAME="Emory Merryman" -p 28080:8080  --volume ${HOME}/.private/secret.key:/opt/rapidbulldozer/gpg.secret.key:ro desertedscorpion/cloud9:${VERSION}
+docker run -it --rm --env GPG_SECRET_KEY="${GPG_SECRET_KEY}" --env GPG_OWNER_TRUST="${GPG_OWNER_TRUST}" --env ID_RSA="${ID_RSA}" --env PASS_HOST="${PASS_HOST}" --env PASS_URL="${PASS_URL}" --env CANONICAL_HOST="${CANONICAL_HOST}" --env ORIGIN_HOST="${ORIGIN_HOST}" --env CANONICAL_URL="${CANONICAL_URL}" --env ORIGIN_URL="${ORIGIN_URL}" --env DISPLAY="${DISPLAY}" --env EMAIL="emory.merryman@gmail.com" --env NAME="Emory Merryman" -p ${PORT}:8080  --volume ${HOME}/.private/secret.key:/opt/rapidbulldozer/gpg.secret.key:ro desertedscorpion/cloud9:${VERSION}
 true
