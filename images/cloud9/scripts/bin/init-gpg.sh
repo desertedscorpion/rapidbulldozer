fbe0a29b06a779f7953ce3323e183dd7579c678a
Emory Merryman <emory.merryman@deciphernow.com>
2016-07-04 05:10:35 +0000

    removed gpg cruft

--- images/cloud9/scripts/bin/init-gpg.sh
+++ images/cloud9/scripts/bin/init-gpg.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-gpg --import /opt/rapidbulldozer/properties/gpg.secret.key &&
-(gpg --allow-secret-key --import /opt/rapidbulldozer/properties/gpg.secret.key|| true ) &&
+(gpg --allow-secret-key --import /opt/rapidbulldozer/properties/gpg.secret.key || true ) &&
 (gpg2 --allow-secret-key --import /opt/rapidbulldozer/properties/gpg.secret.key || true ) &&
 gpg --import-ownertrust /opt/rapidbulldozer/properties/gpg.owner.trust &&
 true
