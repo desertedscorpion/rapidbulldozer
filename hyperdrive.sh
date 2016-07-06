08d26ad2078805f538e78b159a5f4dac12871e3b
Emory Merryman <emory.merryman@gmail.com>
2016-07-06 14:19:49 +0000

    adding 2 drive projects

---
+++ hyperdrive.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+CLOUD9_PORT=${1} &&
+NGINX_PORT=${2} &&
+VERSION=$(cat version) &&
+export GPG_SECRET_KEY="$(cat ${HOME}/.private/secret.key)" &&
+export GPG_OWNER_TRUST="$(cat ${HOME}/.private/owner.trust)" &&
+export ID_RSA="$(cat ${HOME}/.ssh/id_rsa)" &&
+export PASS_HOST="github.com" &&
+export PASS_URL=git@github.com:AFnRFCb7/lanterngold.git &&
+export WORKSPACE=$(mktemp -d) &&
+export CANONICAL_HOST=gitlab.363-283.io &&
+export ORIGIN_HOST=gitlab.363-283.io &&
+export CANONICAL_URL=ssh://git@gitlab.363-283.io:2252/cte/hyperdrive.git &&
+export ORIGIN_URL=ssh://git@gitlab.363-283.io:2252/emory.merryman/hyperdrive.git &&
+export DISPLAY=$(ifconfig | grep "inet.*netmask" | grep -v "127.0.0.1" | cut -f2 -d " "):0 &&
+export EMAIL=emory.merryman@gmail.com &&
+export NAME="Emory Merryman" &&
+source ${HOME}/.private/ldap.properties &&
+export LDAP_USERNAME=${LDAP_USERNAME} &&
+export LDAP_PASSWORD=${LDAP_PASSWORD} &&
+export LDAP_EMAIL=${LDAP_EMAIL} &&
+source ${HOME}/.private/github.properties &&
+export GITHUB_ACCESS_TOKEN=${GITHUB_PERSONAL_ACCESS_TOKEN} &&
+docker run -it --rm \
+    --env GPG_SECRET_KEY="${GPG_SECRET_KEY}" --env GPG_OWNER_TRUST="${GPG_OWNER_TRUST}" --env ID_RSA="${ID_RSA}" --env PASS_HOST="${PASS_HOST}" \
+    --env PASS_URL="${PASS_URL}" --env CANONICAL_HOST="${CANONICAL_HOST}" --env ORIGIN_HOST="${ORIGIN_HOST}" --env CANONICAL_URL="${CANONICAL_URL}" \
+    --env ORIGIN_URL="${ORIGIN_URL}" --env DISPLAY="${DISPLAY}" --env EMAIL="emory.merryman@deciphernow.com" --env NAME="Emory Merryman" \
+    --env LDAP_USERNAME="${LDAP_USERNAME}" --env LDAP_PASSWORD="${LDAP_PASSWORD}" --env LDAP_EMAIL="${LDAP_EMAIL}" \
+    --env GITHUB_ACCESS_TOKEN="${GITHUB_ACCESS_TOKEN}" \
+    -p ${CLOUD9_PORT}:8080 -p ${NGINX_PORT}:8443  --volume ${HOME}/.private/secret.key:/opt/rapidbulldozer/gpg.secret.key:ro desertedscorpion/cloud9:${VERSION}
+true
